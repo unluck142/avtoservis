839f4cf503b0611c9f7603a7c8bf04a64e270f9a
unluck142 <Kachkaryov2010@gmail.com>
2025-04-29 16:57:08 +0700

    Новая база данных

--- storage/is221.sql
+++ storage/is221.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Апр 21 2025 г., 09:20
+-- Время создания: Апр 29 2025 г., 11:55
 -- Версия сервера: 10.4.32-MariaDB
 -- Версия PHP: 8.2.12
 
@@ -24,6 +24,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `appointments`
+--
+
+CREATE TABLE `appointments` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `bookings`
+--
+
+CREATE TABLE `bookings` (
+  `bookingid` int(11) NOT NULL,
+  `usersid` int(11) DEFAULT NULL,
+  `productsid` int(11) DEFAULT NULL,
+  `bookingDate` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `orders`
 --
 
@@ -34,8 +60,17 @@ CREATE TABLE `orders` (
   `phone` varchar(15) DEFAULT NULL,
   `email` varchar(120) DEFAULT NULL,
   `all_sum` float NOT NULL,
-  `created` datetime DEFAULT current_timestamp()
+  `created` datetime DEFAULT current_timestamp(),
+  `user_id` int(11) DEFAULT 0,
+  `status` int(11) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `orders`
+--
+
+INSERT INTO `orders` (`id`, `fio`, `address`, `phone`, `email`, `all_sum`, `created`, `user_id`, `status`) VALUES
+(1, 'качкарёв', 'Патриотов', '89069756331', 'kachkaryov2010@gmail.com', 0, '2025-04-29 03:17:14', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -75,7 +110,13 @@ CREATE TABLE `products` (
 INSERT INTO `products` (`id`, `name`, `description`, `image`, `price`, `created`, `updated`) VALUES
 (1, 'Проверка машини', 'Проверяем вашу машину на проблемы', 'https://localhost/avtoservis/assets/images/proverka.png', 6999, '2025-04-07 12:45:45', '2025-04-08 10:34:37'),
 (2, 'Замена масла', 'Заменяем масло', 'https://localhost/avtoservis/assets/images/oil.png', 6000, '2025-04-07 12:45:45', '2025-04-08 10:34:15'),
-(3, 'Ремонт', 'Ремонтируем вашу машину', 'ttps://localhost/avtoservis/assets/images/remont.png', 4000, '2025-04-07 12:45:45', '2025-04-08 10:35:31');
+(3, 'Ремонт', 'Ремонтируем вашу машину', 'https://localhost/avtoservis/assets/images/remont.png', 4000, '2025-04-07 12:45:45', '2025-04-24 21:39:14');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
@@ -84,11 +125,39 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `token` varchar(255) NOT NULL,
   `is_verified` tinyint(1) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `address` varchar(255) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `token`, `is_verified`, `created_at`, `address`, `phone`) VALUES
+(1, 'unluck', 'slump55992@credit-loans.xyz', '$2y$10$/6N6i/WghPLXCsosr2oPhOhllBit66jmLCl6CDz7mqmcUduwrJ3Yi', '', 1, '2025-04-24 21:05:15', NULL, NULL),
+(2, 'admin', 'kachkaryov2010@gmail.com', '$2y$10$e4HuMUyCZVEIn.HhnCKi6ejqBBZ6k2erBISpPEzv4w6xJYW4qmuYW', '', 1, '2025-04-24 21:10:45', NULL, NULL),
+(3, 'admin', 'adult966@business-degree.live', '$2y$10$jlty5ROw.kwgsU2GZMGbtOLZnoYGfgVbqUuiDoyav4V0B/ZJ4EeL6', '', 1, '2025-04-25 15:12:46', NULL, NULL),
+(5, 'unluck142', 'plot9743@examstudy.xyz', '$2y$10$WJtk.jthoOoFAcMSu1twtugicKMlmUXnU8sx.wWRG.56.ip.uiRZe', '', 1, '2025-04-29 16:07:17', NULL, NULL);
+
 --
 -- Индексы сохранённых таблиц
 --
+
+--
+-- Индексы таблицы `appointments`
+--
+ALTER TABLE `appointments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Индексы таблицы `bookings`
+--
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`bookingid`),
+  ADD KEY `usersid` (`usersid`),
+  ADD KEY `productsid` (`productsid`);
 
 --
 -- Индексы таблицы `orders`
@@ -112,14 +181,32 @@ ALTER TABLE `products`
   ADD UNIQUE KEY `name` (`name`);
 
 --
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT для сохранённых таблиц
 --
+
+--
+-- AUTO_INCREMENT для таблицы `appointments`
+--
+ALTER TABLE `appointments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `bookings`
+--
+ALTER TABLE `bookings`
+  MODIFY `bookingid` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT для таблицы `order_item`
@@ -134,8 +221,27 @@ ALTER TABLE `products`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- Ограничения внешнего ключа сохраненных таблиц
 --
+
+--
+-- Ограничения внешнего ключа таблицы `appointments`
+--
+ALTER TABLE `appointments`
+  ADD CONSTRAINT `appointments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `bookings`
+--
+ALTER TABLE `bookings`
+  ADD CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`usersid`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `bookings_ibfk_2` FOREIGN KEY (`productsid`) REFERENCES `products` (`id`);
 
 --
 -- Ограничения внешнего ключа таблицы `order_item`
